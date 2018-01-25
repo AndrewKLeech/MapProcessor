b7010f94905f32fb9a800d10fa232088c0c43de1
unknown <andrewkleech@gmail.com>
2018-01-25 18:44:40 +0000

    make app display full image taken from camera

--- app/src/main/java/com/example/andrew/mapprocessor/MainActivity.kt
+++ app/src/main/java/com/example/andrew/mapprocessor/MainActivity.kt
@@ -1,27 +1,72 @@
 package com.example.andrew.mapprocessor
 
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.provider.MediaStore
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
 import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import java.io.File
-import java.net.URI
 import android.os.StrictMode
+import android.support.v4.content.FileProvider
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
+import android.graphics.BitmapFactory
 
 
 
 class MainActivity : AppCompatActivity() {
+    var mCurrentPhotoPath: String? = null
+    var CAM_INTENT = 1;
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val imageFileName = "JPEG_" + timeStamp + "_"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val image = File.createTempFile(
+                imageFileName, /* prefix */
+                ".jpg", /* suffix */
+                storageDir      /* directory */
+        )
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.absolutePath
+
+        return image
+    }
+
+    private fun setPic() {
+        // Get the dimensions of the View
+        Toast.makeText(this, "Setting picture", Toast.LENGTH_SHORT).show()
+        val targetW = captured_map_image.getWidth()
+        val targetH = captured_map_image.getHeight()
+
+        // Get the dimensions of the bitmap
+        val bmOptions = BitmapFactory.Options()
+        bmOptions.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions)
+        val photoW = bmOptions.outWidth
+        val photoH = bmOptions.outHeight
+
+        // Determine how much to scale down the image
+        val scaleFactor = Math.min(photoW / targetW, photoH / targetH)
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false
+        bmOptions.inSampleSize = scaleFactor
+        bmOptions.inPurgeable = true
+
+        val bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions)
+        captured_map_image.setImageBitmap(bitmap)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,30 +74,35 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         val builder = StrictMode.VmPolicy.Builder()
         StrictMode.setVmPolicy(builder.build())
-        capture_btn.setOnClickListener{
-
+        capture_btn.setOnClickListener {
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
             if (takePictureIntent.resolveActivity(packageManager) != null) {
-                startActivityForResult(takePictureIntent, 1)
+                // Create the File where the photo should go
+                var photoFile: File? = null
+                try {
+                    photoFile = createImageFile()
+                }
+                catch (e:IOException) {
+                    Toast.makeText(this, "Error occured while creating File", Toast.LENGTH_SHORT).show()
+                }
+
+                // Continue only if the File was successfully created
+                if (photoFile != null) {
+                    var photoURI: Uri = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photoFile)
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, CAM_INTENT)
+                }
+
             }
-
-            Toast.makeText(this, "yo", Toast.LENGTH_SHORT).show()}
-
-    }
-
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        //converted form https://developer.android.com/training/camera/photobasics.html
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            var extras: Bundle? = data?.getExtras()
-            var imageBitmap: Bitmap? = null
-            var dataGot = extras?.get("data")
-            if (dataGot is Bitmap) {
-                imageBitmap = dataGot
-            }
-            captured_map_image.setImageBitmap(imageBitmap)
         }
     }
+
+    override fun onActivityResult(requestCode: Int,  resultCode: Int, data: Intent?){
+        if(requestCode == CAM_INTENT){
+            setPic()
+        }
+    }
+
     //menu
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
