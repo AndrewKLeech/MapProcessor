1d67d779ab37ff756a0c0b37a807ec64f09ab3fc
unknown <andrewkennethleech@gmail.com>
2018-03-27 18:57:51 +0100

    Add c++ support, also change lots of structure stuff because I broke the whole project in an update

---
+++ app/src/main/java/com/example/andrew/maps/ConvertActivity.kt
@@ -0,0 +1,178 @@
+package com.example.andrew.maps
+
+import org.opencv.core.Mat
+import android.os.Bundle
+import android.os.StrictMode
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import kotlinx.android.synthetic.main.convert_screen.*
+import android.widget.SeekBar
+import org.opencv.android.OpenCVLoader
+import org.opencv.android.Utils
+import org.opencv.core.Core
+import org.opencv.core.Scalar
+
+
+import org.opencv.imgproc.Imgproc
+
+/**
+* Created by Andrew on 28/01/2018.
+*/
+
+class ConvertActivity : AppCompatActivity() {
+
+    private var mSrcPhotoPath: String? = null
+    var mCurrentPhotoPath: String? = null
+    // Bitmap of segmented image
+    var bitmapconv: Bitmap? = null
+    var photoFile: File? = null
+    var hsV_lower = 89.0
+    var hsV_upper = 106.0
+    var hSv_lower = 0.0
+
+    private fun setPic(path: String) {
+        // Get the dimensions of the View
+        val targetW = convert_map_img_view.width
+        val targetH = convert_map_img_view.height
+
+        // Get the dimensions of the bitmap
+        val bmOptions = BitmapFactory.Options()
+        bmOptions.inJustDecodeBounds = true
+        BitmapFactory.decodeFile(path, bmOptions)
+        val photoW = bmOptions.outWidth
+        val photoH = bmOptions.outHeight
+        // Determine how much to scale down the image
+        val scaleFactor = Math.min(photoW / targetW, photoH / targetH)
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false
+        bmOptions.inSampleSize = scaleFactor
+        bmOptions.inPurgeable = true
+
+        val bitmap = BitmapFactory.decodeFile(path, bmOptions)
+        val bitmap_cpy = bitmap.copy(bitmap.config, true)
+        bitmapconv =  PrepImage(bitmap_cpy)
+
+        convert_map_img_view.setImageBitmap(bitmapconv)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        if (!OpenCVLoader.initDebug()) {
+            // Handle initialization error
+        }
+        val intent = intent
+        mSrcPhotoPath = intent.getStringExtra("src")
+        mCurrentPhotoPath = intent.getStringExtra("img")
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.convert_screen)
+        setSupportActionBar(toolbar)
+        val builder = StrictMode.VmPolicy.Builder()
+        StrictMode.setVmPolicy(builder.build())
+
+        // Set starting progress of Value min bar to hsV_lower
+        valLowerSeekBar.progress = hsV_lower.toInt()
+        // Set starting progress of Value max bar to hsV_upper
+        valUpperSeekBar.progress = hsV_upper.toInt()
+
+        valLowerSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+                mCurrentPhotoPath = mSrcPhotoPath
+                hsV_lower = valLowerSeekBar.progress.toDouble()
+                setPic(mCurrentPhotoPath!!)
+                Log.d("I", "Value is: " + hsV_lower.toString())
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // TODO Auto-generated method stub
+            }
+        })
+        valUpperSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+                mCurrentPhotoPath = mSrcPhotoPath
+                hsV_upper = valUpperSeekBar.progress.toDouble()
+                setPic(mCurrentPhotoPath!!)
+                Log.d("I", "Value is: " + hsV_upper.toString())
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                // TODO Auto-generated method stub
+            }
+        })
+        done_seg_img_btn.setOnClickListener({
+            thin(bitmapconv!!)
+        })
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            setPic(mCurrentPhotoPath!!)
+        }
+
+    }
+
+    fun PrepImage(src: Bitmap): Bitmap {
+
+        var cpy = src.copy(src.config, true)
+        var pixels = IntArray(cpy.height *cpy.width)
+        cpy.getPixels(pixels, 0, cpy.width, 0, 0, cpy.width, cpy.height)
+
+        //http://bagawerexecinux.cf/1305539/6566619/137c6080a-android-colorrgb-to-hsv-83344
+        var mat = Mat()
+        var cpyMat = Mat()
+        var equCpy = Mat()
+        var findBlack = Mat()
+        var invMat = Mat()
+
+        val bmp32 = src.copy(Bitmap.Config.ARGB_8888, true)
+        Utils.bitmapToMat(bmp32, mat)
+        Imgproc.cvtColor(mat, cpyMat, Imgproc.COLOR_BGR2HSV, 3) //3 is HSV Channel
+
+        cpyMat.copyTo(findBlack)
+        cpyMat.copyTo(equCpy)
+        cpyMat.copyTo(invMat)
+
+        // Try get features that we will remove from the image
+        Core.inRange(equCpy, Scalar(0.0, hSv_lower, hsV_lower), Scalar(180.0, 255.0, hsV_upper), findBlack)
+
+        // Invert segmentation
+        //Core.bitwise_not(findBlack,invMat)
+
+        // Turn mat into bitmap to display in app
+        Utils.matToBitmap(findBlack, cpy)
+        return cpy
+    }
+
+    fun thin(bitmap: Bitmap){
+        System.out.println("run thin")
+        var mat = Mat()
+
+        System.out.println("run bitmatToMat")
+        Utils.bitmapToMat(bitmap, mat)
+        var channels = List<Mat>(4, {Mat()})
+        Core.split(mat,channels)
+        var ch1 = channels[0]
+        System.out.println("run Thinner.thin")
+        var thinnedMat = Thinner().thin(ch1)
+
+        System.out.println("matToBitmap")
+        var newBitmap = bitmap.copy(bitmap.config,true)
+        Utils.matToBitmap(thinnedMat, newBitmap)
+
+        convert_map_img_view.setImageBitmap(newBitmap)
+    }
+}
---
+++ app/src/main/java/com/example/andrew/maps/Preview.kt
@@ -0,0 +1,139 @@
+package com.example.andrew.maps
+
+import android.content.Context
+import android.graphics.Rect
+import android.graphics.RectF
+import android.hardware.Camera
+import android.util.Log
+import android.view.MotionEvent
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
+import java.io.IOException
+import android.opengl.ETC1.getHeight
+import android.opengl.ETC1.getWidth
+import kotlinx.android.synthetic.main.preview.view.*
+
+
+@Suppress("NAME_SHADOWING")
+/**
+ * Created by Andrew on 05/03/2018.
+ * whit help form http://blog.rhesoft.com/2015/04/02/tutorial-how-to-use-camera-with-android-and-android-studio/
+ * and https://developer.android.com/training/camera/cameradirect.html#TaskSettings
+ */
+
+class Preview(context: Context, camera:Camera) : SurfaceView(context), SurfaceHolder.Callback {
+
+    var mHolder:SurfaceHolder? = null
+    var mCamera:Camera? = null
+
+    init{
+        mCamera = camera
+        Log.d("SUCCESS", "mCamera is " + mCamera)
+        mCamera!!.setDisplayOrientation(90)
+        mHolder = holder
+        mHolder!!.addCallback(this)
+        mHolder!!.setType(SurfaceHolder.SURFACE_TYPE_NORMAL)
+    }
+    // help from https://stackoverflow.com/questions/18460647/android-setfocusarea-and-auto-focus
+    override fun onTouchEvent(motionEvent: MotionEvent): Boolean {
+        if (mCamera != null) {
+            mCamera!!.cancelAutoFocus()
+            var focusRect: Rect = calculateTapArea(motionEvent.getX(), motionEvent.getY(), 1f);
+
+            var parameters: Camera.Parameters = mCamera!!.getParameters();
+            if (parameters.focusMode.equals(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                parameters.focusMode = Camera.Parameters.FOCUS_MODE_AUTO;
+            }
+
+            if (parameters.getMaxNumFocusAreas() > 0) {
+                var mylist: ArrayList<Camera.Area> = ArrayList()
+                mylist.add(Camera.Area(focusRect, 1000))
+                parameters.focusAreas = mylist
+            }
+
+            try {
+                mCamera!!.cancelAutoFocus();
+                mCamera!!.parameters = parameters;
+                mCamera!!.startPreview()
+                mCamera!!.autoFocus({ b: Boolean, camera: Camera ->
+                    @Override
+                    fun onAutoFocus(success: Boolean, camera: Camera) {
+                        if (camera.parameters.focusMode == Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE) {
+                            val parameters: Camera.Parameters = camera.parameters;
+                            parameters.focusMode = Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE;
+                            if (parameters.maxNumFocusAreas > 0) {
+                                parameters.focusAreas = null
+                            }
+                            camera.parameters = parameters
+                            camera.startPreview()
+                        }
+                    }
+                })
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        return true
+    }
+
+
+
+    override fun surfaceCreated(surfaceHolder: SurfaceHolder?) {
+       try{
+           //when the surface is created, set the camera to draw images here
+           mCamera!!.setPreviewDisplay(surfaceHolder)
+           mCamera!!.startPreview()
+       } catch (e:IOException){
+           Log.d("ERROR", "Camera error on surfaceCreated " + e.message)
+       }
+    }
+
+
+    override fun surfaceDestroyed(p0: SurfaceHolder?) {
+        mCamera!!.stopPreview()
+        mCamera!!.release()
+    }
+
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+        //before changing the application orientation, preview needs to be stopped
+        if(mHolder!!.surface == null)/*check if surface is ready to recieve*/{
+            return
+        }
+        try{
+            mCamera!!.stopPreview()
+        } catch (e:Exception){
+            Log.d("ERROR", "Camera error on surfaceDestroyed while trying to stop Preview " + e.message)
+        }
+
+        //recreate the camera preview
+        try{
+            mCamera!!.setPreviewDisplay(mHolder)
+            mCamera!!.startPreview()
+        } catch (e:IOException){
+            Log.d("ERROR", "Camera error on surfaceCreated while trying to set or start preview " + e.message)
+        }
+    }
+
+    fun clamp(x: Int, min: Int, max: Int): Int {
+        if (x > max) {
+            return max
+        }
+        return if (x < min) {
+            min
+        } else x
+    }
+    fun calculateTapArea(x: Float, y: Float, coefficient: Float): Rect {
+        val areaSize = java.lang.Float.valueOf(210 * coefficient).toInt()
+
+        val left = clamp(x.toInt() - areaSize / 2, 0, this.width - areaSize)
+        val top = clamp(y.toInt() - areaSize / 2, 0, this.height - areaSize)
+
+        val rectF = RectF(left.toFloat(), top.toFloat(), left.toFloat() + areaSize, top.toFloat() + areaSize)
+        matrix.mapRect(rectF)
+
+        return Rect(Math.round(rectF.left), Math.round(rectF.top), Math.round(rectF.right), Math.round(rectF.bottom))
+    }
+
+
+}
